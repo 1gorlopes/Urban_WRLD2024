f63b7bb06b1821373ce4b4bd4441d821ce14a9ac
1gorlopes <145722276+1gorlopes@users.noreply.github.com>
2024-06-04 17:13:41 -0300

    Add files via upload

--- filtrar.js
+++ filtrar.js
@@ -1,6 +1,28 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-    console.log("Digitaram no campo");
-})
+    var clientes = document.querySelectorAll(".cliente");
+
+    if(this.value. length > 0){
+        for (var i = 0; i < clientes.length; i++) {
+            var cliente = clientes [i];
+            var tdNome = cliente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value,"i");
+
+            if (!expressao.test(nome)) {
+                cliente.classList.add("invisivel");
+            } else {
+                cliente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < clientes.length; i++) {
+            var cliente = clientes [i];
+
+            cliente.classList.remove("invisivel");
+        }
+    }
+
+});
 
